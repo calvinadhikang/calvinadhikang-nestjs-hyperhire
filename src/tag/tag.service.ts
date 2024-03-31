ccd641e8f0b4a7fe29ccc24ac6298583e47a4a52
calvinadhikang <calvinadhikang@gmail.com>
2024-03-31 20:05:49 +0700

    add tag feature

--- src/tag/tag.service.ts
+++ src/tag/tag.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Tag } from 'src/entities/tag.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class TagService {}
+export class TagService {
+    constructor(
+        @InjectRepository(Tag)
+        private readonly tagRepository: Repository<Tag>
+    ) {} 
+    
+    async findAll(){
+        return await this.tagRepository.find();
+    }
+}
